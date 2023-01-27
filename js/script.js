89e339a64f3b7294f82973fa91c999cd7f195950
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-01-28 01:12:08 +0200

    corrected password confirmation

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const patterns = {
     firstName: /\w/,
     lastName: /\w/,
     password: /^[\d\w@-]{8,20}$/i,
-    confPassword: /^[\d\w@-]{8,20}$/i,
-    phoneNumber: /^[0][7][0,9]{8}&/,
+    confirmPassword: /^[\d\w@-]{8,20}$/i,
+    phoneNumber: /^[0][7][0-9]{10}&/,
     email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
     //             yourname @ domain   .  com          ( .uk )
 };
