e477f1e7f7831947462cb165397e7d466bc5a9fc
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-01-28 01:03:49 +0200

    regex patterns

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const textCells = document.querySelectorAll('input');
 
+
+// regex patterns
+const patterns = {
+    firstName: /\w/,
+    lastName: /\w/,
+    password: /^[\d\w@-]{8,20}$/i,
+    confPassword: /^[\d\w@-]{8,20}$/i,
+    phoneNumber: /^[0][7][0,9]{8}&/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+    //             yourname @ domain   .  com          ( .uk )
+};
+
+
 textCells.forEach((cell) => {
     cell.addEventListener('keyup',(e) => {
         console.log(cell.className);
